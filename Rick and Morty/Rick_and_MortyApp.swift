f057f6cfe82e8b3854d56b106d9b310078918636
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2022-02-16 22:30:16 +0300

    Initial Commit

---
+++ Rick and Morty/Rick_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by Любовь Волкова on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
